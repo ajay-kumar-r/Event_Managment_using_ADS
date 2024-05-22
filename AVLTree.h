73af3173c4f36b7fd6bd0ed7f3857250d02cad2b
Ajay Kumar R <152802348+ajay-kumar-r@users.noreply.github.com>
2024-05-22 14:14:45 +0530

    Update AVLTree.h (Delete)

--- AVLTree.h
+++ AVLTree.h
@@ -1,43 +1,56 @@
-#ifndef AVL_TREE_H
-#define AVL_TREE_H
+#ifndef AVLTREE_H
+#define AVLTREE_H
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <fstream>
-#include <functional>
+#include <ncurses.h>
 
-struct Event {
+using namespace std;
+
+class Event {
+public:
     int id;
-    std::string name;
-    std::string date;
-    std::string startTime;
-    std::string endTime;
+    string name;
+    string date;
+    string startTime;
+    string endTime;
 
-    Event() = default;
-
-    Event(int i, const std::string& n, const std::string& d, const std::string& s, const std::string& e)
-        : id(i), name(n), date(d), startTime(s), endTime(e) {}
+    Event(int id = 0, string name = "", string date = "", string startTime = "", string endTime = "")
+        : id(id), name(name), date(date), startTime(startTime), endTime(endTime) {}
 
     bool operator<(const Event& other) const {
         if (date != other.date) return date < other.date;
-        return startTime < other.startTime;
+        if (startTime != other.startTime) return startTime < other.startTime;
+        return endTime < other.endTime;
     }
 
     bool operator>(const Event& other) const {
-        if (date != other.date) return date > other.date;
-        return startTime > other.startTime;
+        return other < *this;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Event& event) {
-        os << event.id << " " << event.name << " " << event.date << " " << event.startTime << " " << event.endTime;
-        return os;
+    bool operator==(const Event& other) const {
+        return id == other.id;
     }
 
-    friend std::istream& operator>>(std::istream& is, Event& event) {
-        is >> event.id >> event.name >> event.date >> event.startTime >> event.endTime;
-        return is;
+    bool operator!=(const Event& other) const {
+        return !(*this == other);
     }
+
+    friend ostream& operator<<(ostream& os, const Event& event);
+    friend istream& operator>>(istream& is, Event& event);
 };
+
+ostream& operator<<(ostream& os, const Event& event) {
+    os << event.id << " " << event.name << " " << event.date << " " << event.startTime << " " << event.endTime;
+    return os;
+}
+
+istream& operator>>(istream& is, Event& event) {
+    is >> event.id >> event.name >> event.date >> event.startTime >> event.endTime;
+    return is;
+}
 
 struct AVLNode {
     Event event;
@@ -45,179 +58,184 @@ struct AVLNode {
     AVLNode* right;
     int height;
 
-    AVLNode(Event e) : event(e), left(nullptr), right(nullptr), height(1) {}
+    AVLNode(const Event& event, AVLNode* lt, AVLNode* rt, int h = 0)
+        : event(event), left(lt), right(rt), height(h) {}
 };
 
 class AVLTree {
-private:
-    AVLNode* root;
-
-    int height(AVLNode* node) {
-        return node ? node->height : 0;
-    }
-
-    int balanceFactor(AVLNode* node) {
-        return node ? height(node->left) - height(node->right) : 0;
-    }
-
-    void updateHeight(AVLNode* node) {
-        if (node) {
-            node->height = 1 + std::max(height(node->left), height(node->right));
-        }
-    }
-
-    AVLNode* rotateRight(AVLNode* y) {
-        AVLNode* x = y->left;
-        AVLNode* T2 = x->right;
-
-        x->right = y;
-        y->left = T2;
-
-        updateHeight(y);
-        updateHeight(x);
-
-        return x;
-    }
-
-    AVLNode* rotateLeft(AVLNode* x) {
-        AVLNode* y = x->right;
-        AVLNode* T2 = y->left;
-
-        y->left = x;
-        x->right = T2;
-
-        updateHeight(x);
-        updateHeight(y);
-
-        return y;
-    }
-
-    AVLNode* balance(AVLNode* node) {
-        if (!node) return node;
-
-        updateHeight(node);
-
-        int balance = balanceFactor(node);
-
-        if (balance > 1 && balanceFactor(node->left) >= 0) {
-            return rotateRight(node);
-        }
-
-        if (balance > 1 && balanceFactor(node->left) < 0) {
-            node->left = rotateLeft(node->left);
-            return rotateRight(node);
-        }
-
-        if (balance < -1 && balanceFactor(node->right) <= 0) {
-            return rotateLeft(node);
-        }
-
-        if (balance < -1 && balanceFactor(node->right) > 0) {
-            node->right = rotateRight(node->right);
-            return rotateLeft(node);
-        }
-
-        return node;
-    }
-
-    AVLNode* insert(AVLNode* node, Event event) {
-        if (!node) return new AVLNode(event);
-
-        if (event < node->event) {
-            node->left = insert(node->left, event);
-        } else if (event > node->event) {
-            node->right = insert(node->right, event);
-        } else {
-            return node; // Duplicate events are not allowed
-        }
-
-        return balance(node);
-    }
-
-    void inorder(AVLNode* node, std::ostream& os) const {
-        if (node) {
-            inorder(node->left, os);
-            os << node->event << std::endl;
-            inorder(node->right, os);
-        }
-    }
-
-    AVLNode* findMin(AVLNode* node) {
-        while (node->left != nullptr) node = node->left;
-        return node;
-    }
-
-    AVLNode* remove(AVLNode* root, Event event) {
-        if (!root) return root;
-
-        if (event < root->event) {
-            root->left = remove(root->left, event);
-        } else if (event > root->event) {
-            root->right = remove(root->right, event);
-        } else {
-            if (!root->left) {
-                AVLNode* temp = root->right;
-                delete root;
-                return temp;
-            } else if (!root->right) {
-                AVLNode* temp = root->left;
-                delete root;
-                return temp;
-            }
-
-            AVLNode* temp = findMin(root->right);
-            root->event = temp->event;
-            root->right = remove(root->right, temp->event);
-        }
-
-        return balance(root);
-    }
-
 public:
     AVLTree() : root(nullptr) {}
 
-    void insert(Event event) {
-        root = insert(root, event);
+    ~AVLTree() {
+        makeEmpty(root);
     }
 
-    void remove(Event event) {
-        root = remove(root, event);
+    void insert(const Event& event) {
+        insert(event, root);
     }
 
-    void inorder(std::ostream& os) const {
-        inorder(root, os);
+    void remove(int id) {
+        remove(id, root);
     }
 
-    bool detectConflicts(const Event& newEvent) const {
-        return detectConflicts(root, newEvent);
+    bool detectConflicts(const Event& event) const {
+        return detectConflicts(event, root);
     }
 
-    bool detectConflicts(AVLNode* node, const Event& newEvent) const {
-        if (!node) return false;
-
-        if (!(newEvent.endTime <= node->event.startTime || newEvent.startTime >= node->event.endTime)) {
-            return true;
-        }
-
-        return detectConflicts(node->left, newEvent) || detectConflicts(node->right, newEvent);
-    }
-
-    void loadFromFile(const std::string& filename) {
-        std::ifstream infile(filename);
-        if (!infile.is_open()) {
+    void visualize() const {
+        ofstream outfile("avltree.dot");
+        if (!outfile) {
+            cerr << "Error creating dot file" << endl;
             return;
         }
+        outfile << "digraph AVLTree {\n";
+        if (root) {
+            visualize(outfile, root);
+        }
+        outfile << "}\n";
+        outfile.close();
+        system("dot -Tpng avltree.dot -o avltree.png");
+        system("xdg-open avltree.png");
+    }
 
-        Event event;
-        while (infile >> event) {
-            insert(event);
+private:
+    AVLNode* root;
+
+    void insert(const Event& event, AVLNode*& t) {
+        if (t == nullptr) {
+            t = new AVLNode(event, nullptr, nullptr);
+        } else if (event < t->event) {
+            insert(event, t->left);
+            if (height(t->left) - height(t->right) == 2) {
+                if (event < t->left->event) {
+                    rotateWithLeftChild(t);
+                } else {
+                    doubleWithLeftChild(t);
+                }
+            }
+        } else if (event > t->event) {
+            insert(event, t->right);
+            if (height(t->right) - height(t->left) == 2) {
+                if (event > t->right->event) {
+                    rotateWithRightChild(t);
+                } else {
+                    doubleWithRightChild(t);
+                }
+            }
+        }
+        t->height = max(height(t->left), height(t->right)) + 1;
+    }
+
+    void remove(int id, AVLNode*& t) {
+        if (t == nullptr) {
+            return;
+        }
+        if (id < t->event.id) {
+            remove(id, t->left);
+        } else if (id > t->event.id) {
+            remove(id, t->right);
+        } else if (t->left != nullptr && t->right != nullptr) {
+            t->event = findMin(t->right)->event;
+            remove(t->event.id, t->right);
+        } else {
+            AVLNode* oldNode = t;
+            t = (t->left != nullptr) ? t->left : t->right;
+            delete oldNode;
+        }
+        balance(t);
+    }
+
+    bool detectConflicts(const Event& event, AVLNode* t) const {
+        if (t == nullptr) {
+            return false;
+        }
+        if (event.date == t->event.date && 
+            !(event.endTime <= t->event.startTime || event.startTime >= t->event.endTime)) {
+            return true;
+        }
+        return detectConflicts(event, t->left) || detectConflicts(event, t->right);
+    }
+    void visualize(ofstream& outfile, AVLNode* t) const {
+        if (t->left) {
+            outfile << "\"" << t->event.name << "\\n" << t->event.date << "\\n" << t->event.startTime << "-" << t->event.endTime << "\" -> \"" << t->left->event.name << "\\n" << t->left->event.date << "\\n" << t->left->event.startTime << "-" << t->left->event.endTime << "\";\n";
+            visualize(outfile, t->left);
+        }
+        if (t->right) {
+            outfile << "\"" << t->event.name << "\\n" << t->event.date << "\\n" << t->event.startTime << "-" << t->event.endTime << "\" -> \"" << t->right->event.name << "\\n" << t->right->event.date << "\\n" << t->right->event.startTime << "-" << t->right->event.endTime << "\";\n";
+            visualize(outfile, t->right);
         }
     }
 
-    void saveToFile(const std::string& filename) const {
-        std::ofstream outfile(filename);
-        inorder(outfile);
+    int height(AVLNode* t) const {
+        return t == nullptr ? -1 : t->height;
+    }
+
+    void rotateWithLeftChild(AVLNode*& k2) {
+        AVLNode* k1 = k2->left;
+        k2->left = k1->right;
+        k1->right = k2;
+        k2->height = max(height(k2->left), height(k2->right)) + 1;
+        k1->height = max(height(k1->left), k2->height) + 1;
+        k2 = k1;
+    }
+
+    void rotateWithRightChild(AVLNode*& k1) {
+        AVLNode* k2 = k1->right;
+        k1->right = k2->left;
+        k2->left = k1;
+        k1->height = max(height(k1->left), height(k1->right)) + 1;
+        k2->height = max(height(k2->right), k1->height) + 1;
+        k1 = k2;
+    }
+
+    void doubleWithLeftChild(AVLNode*& k3) {
+        rotateWithRightChild(k3->left);
+        rotateWithLeftChild(k3);
+    }
+
+    void doubleWithRightChild(AVLNode*& k1) {
+        rotateWithLeftChild(k1->right);
+        rotateWithRightChild(k1);
+    }
+
+    AVLNode* findMin(AVLNode* t) const {
+        if (t == nullptr) {
+            return nullptr;
+        }
+        if (t->left == nullptr) {
+            return t;
+        }
+        return findMin(t->left);
+    }
+
+    void balance(AVLNode*& t) {
+        if (t == nullptr) {
+            return;
+        }
+        if (height(t->left) - height(t->right) > 1) {
+            if (height(t->left->left) >= height(t->left->right)) {
+                rotateWithLeftChild(t);
+            } else {
+                doubleWithLeftChild(t);
+            }
+        } else if (height(t->right) - height(t->left) > 1) {
+            if (height(t->right->right) >= height(t->right->left)) {
+                rotateWithRightChild(t);
+            } else {
+                doubleWithRightChild(t);
+            }
+        }
+        t->height = max(height(t->left), height(t->right)) + 1;
+    }
+
+    void makeEmpty(AVLNode*& t) {
+        if (t != nullptr) {
+            makeEmpty(t->left);
+            makeEmpty(t->right);
+            delete t;
+        }
+        t = nullptr;
     }
 };
 
-#endif // AVL_TREE_H
+#endif // AVLTREE_H
